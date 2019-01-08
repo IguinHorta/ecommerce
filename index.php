a363b911d4122241ef7e8c59ee8069721b5711e0
Igor Horta <igor123horta@hotmail.com>
2019-01-08 10:13:02 -0200

    Template admin

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 
 $app = new Slim();
@@ -15,6 +16,14 @@ $app->get('/', function() {//O site sem nenhuma rota, executa isso
 	$page = new Page();
 
 	$page -> setTpl("index");
+
+});
+
+$app->get('/admin', function() {//O site sem nenhuma rota, executa isso
+    
+	$pageAdmin = new PageAdmin();
+
+	$pageAdmin -> setTpl("index");
 
 });
 
